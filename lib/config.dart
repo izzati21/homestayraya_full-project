605c708acd1a6fe453a0dc2f62d85766ab58d783
izzati21 <116129716+izzati21@users.noreply.github.com>
2023-02-01 13:44:34 +0800

    Add files via upload

---
+++ lib/config.dart
@@ -0,0 +1,3 @@
+class ServerConfig {
+  static const String SERVER = "http://10.19.41.82/homestayraya";
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+import 'package:homestay_raya/views/screens/splashscreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Homestay Raya',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
